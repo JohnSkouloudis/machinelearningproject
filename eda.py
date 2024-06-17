db34467eba349d8ae58ae30fb931fa835c314b7c
John skoul <johnskoul56@gmail.com>
2024-06-17 19:28:26 +0300

    added comments

--- eda.py
+++ eda.py
@@ -11,11 +11,13 @@ print(f'1.The number of records is: {records}')
 
 num_revenues = (data['Revenue'] == True).sum()
 
+#1. Πόσες είναι οι εγγραφές του συνόλου δεδομένων;
 print(f'The number of revenues is: {num_revenues}')
 
-
+#2. Σε τι ποσοστό από αυτές οι χρήστες αγόρασαν τελικά;
 print(f'2.The percentage of users that bought is: {( ( num_revenues/records ) *100 ):.2f}%')
 
+#3. Ποια είναι η ευστοχία (accuracy) ενός μοντέλου το οποίο προβλέπει πάντα ότι ο χρήστης δε θα αγοράσει (ανεξαρτήτως των χαρακτηριστικών του);
 print(f'Accuracy of a model that always predicts that a user will not buy: {( (data['Revenue'] == False).mean() * 100 ):.2f}%')
 
 
--- linear_classification.py
+++ linear_classification.py
@@ -8,43 +8,52 @@ from sklearn import metrics
 
 def prepare_data(df, train_size=None,shuffle=True, random_state=None):
 
+    #αφαίρεση χαρακτηριστικών
     df = df.drop(['Month', 'Browser', 'OperatingSystems'], axis=1)
 
+    #μετατροπή των τιμών του χαρακτηριστικού 'Revenue' σε ακέραιους
     df['Revenue'] = df['Revenue'].astype(int)
 
+    #One-hot encoding στις μεταβλητές 'Region', 'TrafficType', 'VisitorType'
     df = pd.get_dummies(df, columns=['Region', 'TrafficType', 'VisitorType'])
 
+    #χωρισμός της μεταβλητής Revenue που είναι ο στόχος από τις υπόλοιπες
     X = df.drop('Revenue', axis=1)
-
     y = df['Revenue']
 
+    #χωρισμός των δεδομένων σε σύνολο εκπαίδευσης και σε σύνολο δοκιμής
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size, shuffle=shuffle, random_state=random_state)
 
     return X_train, X_test, y_train, y_test
 
-
+#διάβασμα συνόλου δεδομένων
 df = pd.read_csv('project2_dataset.csv')
-
     
 X_train, X_test, y_train, y_test = prepare_data(df, train_size=0.7, shuffle=True, random_state=42)
 
 scaler = MinMaxScaler()
 
+#γραμμική κανονικοποίηση
 X_train_scaler = scaler.fit_transform(X_train)
 X_test_scaler = scaler.transform(X_test)
 
 lr = LogisticRegression(penalty=None, max_iter=30000)
 lr.fit(X_train_scaler, y_train)
+lr.fit(X_test_scaler, y_test)
 
 y_hat_train = lr.predict(X_train_scaler)
 y_hat_test = lr.predict(X_test_scaler)
 
+# ευστοχία του μοντέλου στο σύνολο εκπαίδευσης
 acc_train = metrics.accuracy_score(y_train, y_hat_train)
+
+# ευστοχία του μοντέλου στο σύνολο δοκιμής
 acc_test = metrics.accuracy_score(y_test, y_hat_test)
 
 print('Accuracy training = {}'.format(acc_train))
 print('Accuracy test = {}'.format(acc_test))
 
+# Πίνακας σύγχυσης
 c_m = metrics.confusion_matrix(y_test, y_hat_test)
 print(c_m)
 
