6536eb432a562b4fde275ba509e977c96ffb61e8
John skoul <johnskoul56@gmail.com>
2024-10-14 04:53:07 +0300

    double quotes inside the f-string

--- eda.py
+++ eda.py
@@ -18,6 +18,6 @@ print(f'The number of revenues is: {num_revenues}')
 print(f'2.The percentage of users that bought is: {( ( num_revenues/records ) *100 ):.2f}%')
 
 #3. Ποια είναι η ευστοχία (accuracy) ενός μοντέλου το οποίο προβλέπει πάντα ότι ο χρήστης δε θα αγοράσει (ανεξαρτήτως των χαρακτηριστικών του);
-print(f'Accuracy of a model that always predicts that a user will not buy: {( (data['Revenue'] == False).mean() * 100 ):.2f}%')
+print(f"Accuracy of a model that always predicts that a user will not buy: {( (data['Revenue'] == False).mean() * 100 ):.2f}%")
 
 
